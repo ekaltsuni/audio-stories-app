18cd86625facbb7f77cd6c7642f3ec38b632d13d
lonerae <78692050+lonerae@users.noreply.github.com>
2024-02-27 11:57:57 +0200

    basic layout for portrait mode

---
+++ app/app/src/main/java/gr/unipi/android/audiostories/StoryActivity.java
@@ -0,0 +1,14 @@
+package gr.unipi.android.audiostories;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+    }
+}
